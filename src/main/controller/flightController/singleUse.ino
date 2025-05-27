04eb0a8107996ff02ed4626f17c6b0efdf67d6de
Aaron <152491862+100542@users.noreply.github.com>
2025-05-27 07:50:44 -0700

    Update singleUse.ino

--- src/main/controller/flightController/singleUse.ino
+++ src/main/controller/flightController/singleUse.ino
@@ -1,10 +1,17 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-constexpr uint8_t ROTOR1_PIN = 5; 
+Servo ESC;
+
+// Servo library voor de ESC communication
+// Hou de opstartfrequentie op de gehanteerde waarde. Dat is lager op hetzelfde als 1ms. 
+
+constexpr uint8_t ROTOR1_PIN = 9; 
 
 constexpr uint8_t THROTTLE_PWM = 75; 
 
 void setup() {
+  ESC.attach(9,1000,2000)
   Serial.begin(115200);
 
   pinMode(ROTOR1_PIN, OUTPUT);
