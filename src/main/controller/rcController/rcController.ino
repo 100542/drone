d26e1d09326ada6295fc74bf94d7c9578515ddd0
100542 <152491862+100542@users.noreply.github.com>
2025-06-11 22:47:09 +0200

    just in case commit (might not work)

--- src/main/controller/rcController/rcController.ino
+++ src/main/controller/rcController/rcController.ino
@@ -1,91 +1,135 @@
-#include <Arduino.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
-constexpr uint8_t RF_CE_PIN  = 8;
-constexpr uint8_t RF_CSN_PIN = 9;
-const byte RADIO_ADDRESS[6] = "00001";
-
-constexpr uint8_t BTN_ARM_PIN      = 5; 
-constexpr uint8_t BTN_THROTTLE_UP  = 6;
-constexpr uint8_t BTN_THROTTLE_DN  = 7;
-
-constexpr uint8_t YAW_PIN   = A0;
-constexpr uint8_t PITCH_PIN = A1;
-constexpr uint8_t ROLL_PIN  = A2;
-
-struct ControlPacket {
-  bool   armed;
-  bool   failsafe;
-  int16_t throttle;  
-  int16_t yaw;       
-  int16_t pitch;    
-  int16_t roll;      
-};
-
-RF24 radio(RF_CE_PIN, RF_CSN_PIN);
-ControlPacket tx{};
-
-unsigned long lastSendMs = 0;
-constexpr uint16_t SEND_INTERVAL_MS = 50;
-
-bool debounceRead(uint8_t pin) {
-  static uint32_t lastChange[20] = {0};
-  static bool lastState[20] = {false};
-  bool state = !digitalRead(pin); 
-  if (state != lastState[pin] && millis() - lastChange[pin] > 25) {
-    lastChange[pin] = millis();
-    lastState[pin] = state;
-  }
-  return lastState[pin];
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <chrono> 
+#include <thread>
+#include <WS2tcpip.h>
+ 
+#ifdef _WIN32
+    #include <winsock2.h>
+    #pragma comment(lib, "ws2_32.lib")
+    using socklen_t = int;
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h> 
+#endif
+ 
+const char* DRONE_IP = "192.168.240.124";
+const unsigned int DRONE_UDP_PORT = 4210;
+ 
+bool init_sockets() {
+#ifdef _WIN32
+    WSADATA wsaData;
+    return WSAStartup(MAKEWORD(2, 2), &wsaData) == 0;
+#else
+    return true;
+#endif
+}
+ 
+void cleanup_sockets() {
+#ifdef _WIN32
+    WSACleanup();
+#endif
 }
 
-int16_t readAxis(uint8_t pin) {
-  int raw = analogRead(pin);    
-  return map(raw, 0, 1023, -500, 500); 
+void close_socket(int sock) {
+#ifdef _WIN32
+    closesocket(sock);
+#else
+    close(sock);
+#endif
 }
-
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(BTN_ARM_PIN, INPUT_PULLUP);
-  pinMode(BTN_THROTTLE_UP, INPUT_PULLUP);
-  pinMode(BTN_THROTTLE_DN, INPUT_PULLUP);
-
-  if (!radio.begin()) {
-    Serial.println(F("NRF24 init failed!"));
-  }
-  radio.openWritingPipe(RADIO_ADDRESS);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.setDataRate(RF24_1MBPS);
-  radio.stopListening();
-
-  Serial.println(F("RC controller ready"));
-}
-
-void loop() {
-  static bool armLatched = false;
-  if (debounceRead(BTN_ARM_PIN)) {
-    if (!armLatched) {  
-      tx.armed = !tx.armed;
-      armLatched = true;
+ 
+int main() {
+    if (!init_sockets()) {
+        std::cerr << "Failed to initialize sockets." << std::endl;
+        return 1;
     }
-  } else {
-    armLatched = false;
-  }
-
-  bool throttleUp   = debounceRead(BTN_THROTTLE_UP);
-  bool throttleDown = debounceRead(BTN_THROTTLE_DN);
-
-  if (throttleUp && tx.throttle < 1000)  tx.throttle += (tx.throttle < 400 ? 5 : 1);
-  if (throttleDown && tx.throttle > 0)   tx.throttle -= (tx.throttle > 400 ? 5 : 1);
-
-  tx.yaw   = readAxis(YAW_PIN);
-  tx.pitch = readAxis(PITCH_PIN);
-  tx.roll  = readAxis(ROLL_PIN);
-
-  if (millis() - lastSendMs >= SEND_INTERVAL_MS) {
-    radio.write(&tx, sizeof(tx));
-    lastSendMs = millis();
-  }
+ 
+    int sockfd;
+    struct sockaddr_in servaddr;
+ 
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        cleanup_sockets();
+        return 1;
+    }
+ 
+    memset(&servaddr, 0, sizeof(servaddr));
+ 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(DRONE_UDP_PORT);
+    if (inet_pton(AF_INET, DRONE_IP, &servaddr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        close_socket(sockfd);
+        cleanup_sockets();
+        return 1;
+    }
+ 
+    std::cout << "RC Controller Started." << std::endl;
+    std::cout << "Sending commands to " << DRONE_IP << ":" << DRONE_UDP_PORT << std::endl;
+    std::cout << "Commands:" << std::endl;
+    std::cout << "  'w': THROTTLE_UP" << std::endl;
+    std::cout << "  's': THROTTLE_DOWN" << std::endl;
+    std::cout << "  'a': MOVE_LEFT" << std::endl;
+    std::cout << "  'd': MOVE_RIGHT" << std::endl;
+    std::cout << "  'q': TURN_LEFT" << std::endl;
+    std::cout << "  'e': TURN_RIGHT" << std::endl;
+    std::cout << "  'l': LAND" << std::endl;
+    std::cout << "  'p': PING (manual)" << std::endl;
+    std::cout << "  'x': Exit" << std::endl;
+    std::cout << "Press Enter after each command." << std::endl;
+ 
+ 
+    std::string command_to_send;
+    std::string input_char;
+ 
+    std::thread ping_thread([&]() {
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            const char* ping_cmd = "PING";
+            sendto(sockfd, ping_cmd, strlen(ping_cmd), 0,
+                   (const struct sockaddr*)&servaddr, sizeof(servaddr));
+        }
+    });
+    ping_thread.detach();
+ 
+    while (true) {
+        std::cout << "> ";
+        std::getline(std::cin, input_char);
+ 
+        if (input_char == "w") {
+            command_to_send = "THROTTLE_UP";
+        } else if (input_char == "s") {
+            command_to_send = "THROTTLE_DOWN";
+        } else if (input_char == "a") {
+            command_to_send = "MOVE_LEFT";
+        } else if (input_char == "d") {
+            command_to_send = "MOVE_RIGHT";
+        } else if (input_char == "q") {
+            command_to_send = "TURN_LEFT";
+        } else if (input_char == "e") {
+            command_to_send = "TURN_RIGHT";
+        } else if (input_char == "l") {
+            command_to_send = "LAND";
+        } else if (input_char == "p") {
+            command_to_send = "PING";
+        } else if (input_char == "x") {
+            std::cout << "Exiting controller." << std::endl;
+            break;
+        } else {
+            std::cout << "Unknown command." << std::endl;
+            continue;
+        }
+ 
+        sendto(sockfd, command_to_send.c_str(), command_to_send.length(), 0,
+               (const struct sockaddr*)&servaddr, sizeof(servaddr));
+        std::cout << "Sent: " << command_to_send << std::endl;
+    }
+ 
+    close_socket(sockfd);
+    cleanup_sockets();
+    return 0;
 }
